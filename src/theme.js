c56d43416d883f63f6241770e89bec61e8e3ca96
Gabriel de Oliveira Chaves <gabrieloliveirachaves754@gmail.com>
2021-11-05 10:57:08 -0300

    feat: assets, font and colors variables

---
+++ src/theme.js
@@ -0,0 +1,16 @@
+export const theme = {
+  colors: {
+    white: '#ffffff',
+    light_gray: '#f8f8f8',
+    black: '#0c0c0c',
+    gray: '#7B7B7B',
+    primary: '#252160',
+    secondary: '#3AFFE5',
+    blue: '#32B5FF',
+    orange: '#FF9900',
+    gradient: '145.09deg, #26E9CF 11.81%, #25CDE1 95.89%' // background: linear-gradient(145.09deg, #26E9CF 11.81%, #25CDE1 95.89%);
+  },
+  fonts: {
+    font: 'Inter'
+  }
+}
